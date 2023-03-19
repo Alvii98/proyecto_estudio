20d0f0342ecb90510d6c2a2f31db19b645f30b60
Alvii98 <alvaro.98@live.com.ar>
2023-03-19 20:53:52 -0300

    footer en mi casa

--- templates/partials/footer_temp.php
+++ templates/partials/footer_temp.php
@@ -1,8 +1,15 @@
 <header class="container-fluid border-top p-3">
-    <div class="col-md-12">
-        <img src="img/logo-Negro.png" width="300px" height="60px" class="col-md-3">
-        <i class="bi bi-envelope col-md-3"></i>
-        <i class="bi bi-instagram col-md-3"></i>
-        <i class="bi bi-whatsapp col-md-3"></i>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="float-left col-md-6">
+                <img src="img/logo-Negro.png" width="300px" height="60px">
+            </div>
+            <div class="float-right col-md-6 mt-3 center">
+                <label class="h6 desarrollador">Datos del desarrollador <i class="bi bi-laptop-fill"></i></label>
+                <i class="bi bi-envelope h2 mr-5" onclick="location.href='mailto:alvaro.98@live.com.ar'"></i>
+                <i class="bi bi-instagram h2 mr-5" onclick="window.open('https://instagram.com/alviicaporaletti?igshid=MTA2NDdkNWE=', '_blank')"></i>
+                <i class="bi bi-whatsapp h2" onclick="window.open('https://api.whatsapp.com/send?phone=5492346571470', '_blank')"></i>
+            </div>
+        </div>
     </div>
 </header>
